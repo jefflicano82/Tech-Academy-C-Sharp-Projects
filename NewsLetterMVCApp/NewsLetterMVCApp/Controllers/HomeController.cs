52de56e92b0e29c4a9224fa19bb29a98e39a2ff6
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-09-27 16:34:01 -0700

    Finished video series

--- NewsLetterMVCApp/NewsLetterMVCApp/Controllers/HomeController.cs
+++ NewsLetterMVCApp/NewsLetterMVCApp/Controllers/HomeController.cs
@@ -1,29 +1,95 @@
-﻿using System;
+﻿using NewsLetterMVCApp.Models;
+using NewsLetterMVCApp.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace NewsLetterMVCApp.Controllers
+namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public int NewsletterSignUp { get; private set; }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
         {
-            ViewBag.Message = "Your application description page.";
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
 
-            return View();
+                string queryString = @"Insert INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                    (@FirstName, @LastName, @EmailAddress)";
+
+                using (SqlConnection connection= new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+
+
+                return View("Success");
+            }
         }
 
-        public ActionResult Contact()
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your contact page.";
+            string queryString = @"Select Id, FirstName, LastName, EmailAddress from Signups";
+            List<NewsletterSignUp> signups=new List<NewsletterSignUp>()
 
+                using (SqlConnection connection=new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlDataReader reader = CommandBehavior.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var signup = new NewsletterSignUp();
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+                    
+                    signups.Add(signup);
+                }
+            }
+
+
+            var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+            {
+                var signupVm = new SignupVm();
+                signupVm.FirstName = signup.FirstName;
+                signupVm.LastName = signup.LastName;
+                signupVm.EmailAddress = signup.EmailAddress;
+                signupVms.Add(signupVm);
+            }
             return View();
         }
     }
---
+++ NewsLetterMVCApp/NewsLetterMVCApp/ViewModels/SignupVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsLetterMVCApp.ViewModels
+{
+    public class SignupVm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+
+    }
+}
