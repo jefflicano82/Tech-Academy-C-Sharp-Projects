8a75e87f1f1b0e175fe53a347c289ea45b17ee5f
jefflicano82 <109245242+jefflicano82@users.noreply.github.com>
2022-10-06 14:22:43 -0700

    added

---
+++ Scores1/Scores1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Scores1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today is {date}.";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\Jeff Licano\Documents\GitHub\Tech-Academy-C-Sharp-Projects\Scores1\Scores1\studentScores1.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double tScore = 0.0;
+
+            Console.WriteLine("\nStudent Score: \n");
+            foreach (string line in lines)
+            {
+                Console.Write("\n" + line);
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotel of " + lines.Length + " student scores. \tAverage score: " + avgScore);
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
